09cc4c062af947922dc26cffb7a6845f48b589ea
naman-seth <sethumans@gmail.com>
2026-04-24 16:22:01 +0530

    components addition

--- pygui.py
+++ pygui.py
@@ -1,7 +1,21 @@
 import tkinter
 
 root = tkinter.Tk()
-root.title("Url short")
+root.title("Url shortner")
 root.geometry('300x150')
+
+# components
+longurl_Label = tkinter.Label(root,text="Enter long url")
+longurl_entry = tkinter.Entry(root)
+shorturl_Label = tkinter.Label(root,text="Enter short url")
+shorturl_entry = tkinter.Entry(root)
+shortButton = tkinter.Button(root,text='Shorten')
+
+#pack to display components on application
+longurl_Label.pack()
+longurl_entry.pack()
+shorturl_Label.pack()
+shorturl_entry.pack()
+shortButton.pack()
 
 root.mainloop()
