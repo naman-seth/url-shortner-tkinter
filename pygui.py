f94ea74fbd9ff692c0d8d4944260577f02dee67d
naman-seth <sethumans@gmail.com>
2026-04-26 00:21:51 +0530

    final push

--- pygui.py
+++ pygui.py
@@ -1,17 +1,22 @@
 import tkinter
+import pyshorteners
 
 root = tkinter.Tk()
 root.title("Url shortner")
 root.geometry('300x150')
 
+def shorten():
+    shortener = pyshorteners.Shortener()
+    shortUrl=shortener.tinyurl.short(longurl_entry.get())  #to get longurl
+    shorturl_entry.insert(0,shortUrl)
 # components
 longurl_Label = tkinter.Label(root,text="Enter long url")
 longurl_entry = tkinter.Entry(root)
 shorturl_Label = tkinter.Label(root,text="Enter short url")
 shorturl_entry = tkinter.Entry(root)
-shortButton = tkinter.Button(root,text='Shorten')
+shortButton = tkinter.Button(root,text='Shorten',command=shorten)
 
-#pack to display components on application
+#pack(geometry manager) to display components on application
 longurl_Label.pack()
 longurl_entry.pack()
 shorturl_Label.pack()
